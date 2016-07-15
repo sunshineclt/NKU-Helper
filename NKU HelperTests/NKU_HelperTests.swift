d7225e84f5c444330a3df89a48eabd76712bcfca
Zac Chan <sunshinecltzac@gmail.com>
2016-07-15 14:14:59 +0800

    持续集成

--- NKU HelperTests/NKU_HelperTests.swift
+++ NKU HelperTests/NKU_HelperTests.swift
@@ -23,6 +23,7 @@ class NKU_HelperTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(1 == 1)
     }
     
     func testPerformanceExample() {
