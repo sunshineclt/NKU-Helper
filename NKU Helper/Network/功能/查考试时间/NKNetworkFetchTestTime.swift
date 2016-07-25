e818bf92116f19b9d4fb811d345ba166d1ddf58c
Zac Chan <sunshinecltzac@gmail.com>
2016-07-26 01:30:43 +0800

    整理NKNetworkFetchTestTime

--- NKU Helper/Network/功能/查考试时间/NKNetworkFetchTestTime.swift
+++ NKU Helper/Network/功能/查考试时间/NKNetworkFetchTestTime.swift
@@ -17,6 +17,7 @@ enum NKNetworkFetchTestTimeResult {
 typealias testTimeArray = [[String:String]]
 
 /// 查询考试时间的网络类
+@objc(NKNetworkFetchTestTime)
 class NKNetworkFetchTestTime: NKNetworkBase {
     
     typealias fetchTestTimeBlock = (NKNetworkFetchTestTimeResult) -> Void
@@ -30,67 +31,42 @@ class NKNetworkFetchTestTime: NKNetworkBase {
      */
     func fetchTestTime(completionHandler: fetchTestTimeBlock) {
         Alamofire.request(.GET, "http://222.30.32.10/xxcx/stdexamarrange/listAction.do").responseString { (response) in
-            if let html = response.result.value {
-                let testTime = self.loadHtml(html)
-                completionHandler(.Success(testTime: testTime))
-            }
-            else {
+            guard let html = response.result.value else {
                 completionHandler(.Fail)
+                return
             }
+            print(html)
+            let testTime = self.loadHtml(html)
+            completionHandler(.Success(testTime: testTime))
         }
     }
     
-    private func loadHtml(html: NSString) -> testTimeArray {
+    dynamic private func loadHtml(html: String) -> testTimeArray {
         var testTime = testTimeArray()
-        do{
-            let regularExp1 = try NSRegularExpression(pattern: "<tr bgcolor=\"#FFFFFF\" >", options: NSRegularExpressionOptions.CaseInsensitive)
-            var resultExp = regularExp1.matchesInString(html as String, options: NSMatchingOptions.ReportProgress, range: NSMakeRange(0, html.length))
-            for i in 0 ..< resultExp.count {
-                let temp:NSString = html.substringWithRange(NSMakeRange(resultExp[i].range.location, 750))
-                //       print("\n**********************\n")
-                //       print(temp)
-                let regularExp2 = try NSRegularExpression(pattern: "(<td align=\"center\" class=\"NavText\">).*?(</td>)", options: NSRegularExpressionOptions.CaseInsensitive)
-                var index = regularExp2.matchesInString(temp as String, options: NSMatchingOptions.ReportProgress, range: NSMakeRange(0, temp.length))
-                var dictionary:[String:String] = [:]
-                for j in 0 ..< index.count {
-                    switch j {
-                    case 1,2,4,5:continue
-                    case 0,3,6,7,8:
-                        let exact:NSString = temp.substringWithRange(index[j].range)
-                        let firstRange = exact.rangeOfString("NavText\">")
-                        let secondRange = exact.rangeOfString("</td>")
-                        let exactNeed:NSString = exact.substringWithRange(NSMakeRange(firstRange.location + 9, secondRange.location - firstRange.location - 9))
-                        switch j {
-                        case 0:
-                            dictionary["className"] = exactNeed as String
-                        case 3:
-                            switch exactNeed {
-                            case "1":dictionary["weekday"] = "星期一"
-                            case "2":dictionary["weekday"] = "星期二"
-                            case "3":dictionary["weekday"] = "星期三"
-                            case "4":dictionary["weekday"] = "星期四"
-                            case "5":dictionary["weekday"] = "星期五"
-                            case "6":dictionary["weekday"] = "星期六"
-                            case "7":dictionary["weekday"] = "星期天"
-                            default:continue
-                            }
-                        case 6:dictionary["classroom"] = exactNeed as String
-                        case 7:
-                            let exactNeedShort = exactNeed.substringWithRange(NSMakeRange(5, 11))
-                            dictionary["startTime"] = exactNeedShort
-                        case 8:
-                            let exactNeedShort = exactNeed.substringWithRange(NSMakeRange(5, 11))
-                            dictionary["endTime"] = exactNeedShort
-                        default:continue
-                        }
-                    default:continue
-                    }
-                }
-                testTime.append(dictionary)
+        let regularExpression = try! NSRegularExpression(pattern: "<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?<td[^<>]*?NavText\">([^<>]*?)</td>\\s*?", options: .DotMatchesLineSeparators)
+        let matches = regularExpression.matchesInString(html, options: .ReportCompletion, range: NSMakeRange(0, (html as NSString).length))
+        let htmlNSString = html as NSString
+        for i in 0..<matches.count {
+            let match = matches[i]
+            var dictionary = [String:String]()
+            dictionary["className"] = htmlNSString.substringWithRange(match.rangeAtIndex(1))
+            dictionary["week"] = htmlNSString.substringWithRange(match.rangeAtIndex(3))
+            switch htmlNSString.substringWithRange(match.rangeAtIndex(4)) {
+            case "1":dictionary["weekday"] = "星期一"
+            case "2":dictionary["weekday"] = "星期二"
+            case "3":dictionary["weekday"] = "星期三"
+            case "4":dictionary["weekday"] = "星期四"
+            case "5":dictionary["weekday"] = "星期五"
+            case "6":dictionary["weekday"] = "星期六"
+            case "7":dictionary["weekday"] = "星期天"
+            default:dictionary["weekday"] = "未知"
             }
-        }
-        catch {
-            return []
+            dictionary["startSection"] = htmlNSString.substringWithRange(match.rangeAtIndex(5))
+            dictionary["endSection"] = htmlNSString.substringWithRange(match.rangeAtIndex(6))
+            dictionary["classroom"] = htmlNSString.substringWithRange(match.rangeAtIndex(7))
+            dictionary["startTime"] = htmlNSString.substringWithRange(match.rangeAtIndex(8))
+            dictionary["endTime"] = htmlNSString.substringWithRange(match.rangeAtIndex(9))
+            testTime.append(dictionary)
         }
         return testTime
     }
